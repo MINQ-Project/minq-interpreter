6ed0d164710740df6f9632f1f447a3d1e6c0e33f
Maciko84 <105291597+Maciko84@users.noreply.github.com>
2024-02-08 14:47:04 +0100

    Add md() to MQHTML Minq

--- src/runtime/modules/web/minqhtmlparser.ts
+++ src/runtime/modules/web/minqhtmlparser.ts
@@ -1,8 +1,17 @@
+import { Marked, marked } from "marked";
 import Parser from "../../../frontend/parser";
 import { createGlobalEnv } from "../../environment";
 import { evaluate } from "../../interpreter";
+import validateArgs from "../../param-checker";
 import { ValueToString } from "../../printer";
-import { FunctionCall, MK_NATIVE_FN, MK_NULL, StringVal } from "../../values";
+import { FunctionCall, MK_NATIVE_FN, MK_NULL, MK_STRING, StringVal } from "../../values";
+import katex from "katex";
+
+const renderer = new marked.Renderer();
+renderer.text = (text: string) => {
+  return katex.renderToString(text, { throwOnError: false });
+};
+marked.use({ renderer });
 
 export default function ParseMQHTML(mqhtml: string, creator: FunctionCall) {
   const pattern = /<\?mq\s(.*?)\?>/gs;
@@ -26,10 +35,21 @@ export default function ParseMQHTML(mqhtml: string, creator: FunctionCall) {
       }),
       true,
     );
+    env.declareVar("md", MK_NATIVE_FN((args, env) => {
+      if(!validateArgs(args, { type: [ "string" ] }))
+      {
+        throw "md(): invaild args"
+      }
+      const htmlString = marked.parse((args[0] as StringVal).value);
+      return MK_STRING(htmlString as string);
+    }), true);
     const newcode = code.substring(4, code.length - 2);
     const ast = parser.produceAST(newcode);
     evaluate(ast, env);
+
     return result;
+   
   });
+
   return newText;
 }
